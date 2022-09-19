2a6c88e091666b0b37565d125a64468074860880
Kurtis Seebaldt <kurtis@thisisartium.com>
2022-09-19 16:30:04 -0500

    Attach policy to deploy user

--- tf/modules/deploy/main.tf
+++ tf/modules/deploy/main.tf
@@ -26,6 +26,11 @@ resource "aws_iam_policy" "deploy_policy" {
   })
 }
 
+resource "aws_iam_user_policy_attachment" "deploy_attach" {
+  user       = aws_iam_user.deploy_user.name
+  policy_arn = aws_iam_policy.deploy_policy.arn
+}
+
 resource "aws_iam_access_key" "deploy_user_access_key" {
   user    = aws_iam_user.deploy_user.name
 }
